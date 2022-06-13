117040d82c9b363537fe8721ef9ad599bd81376f
Unathur <106528488+Unathur@users.noreply.github.com>
2022-06-13 18:28:16 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -211,7 +211,6 @@ class MirrorListener:
             msg += f'\n<b>cc: </b>{self.tag}\n\n'
             if not files:
                 sendMessage(msg, self.bot, self.message)
-                Thread(target=auto_delete_upload_message, args=(bot, self.message, message)).start()
             else:
                 fmsg = ''
                 for index, (link, name) in enumerate(files.items(), start=1):
